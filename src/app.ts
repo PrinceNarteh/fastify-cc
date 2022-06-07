39f3a6184c3376427a20bb15a855517f289147c9
Prince Narteh <prinarteh@gmail.com>
2022-06-07 12:40:35 +0000

    initialize registerUser handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import userRoutes from "./modules/user/user.route";
 
 const server = Fastify();
 
@@ -9,6 +10,9 @@ server.get("/health-check", async () => {
 });
 
 async function main() {
+  // Routes
+  server.register(userRoutes, { prefix: "api/users" });
+
   try {
     const address = await server.listen(3000, "0.0.0.0");
     console.log(`🚀 Server ready at ${address}`);
