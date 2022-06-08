d74506cb5972ebb365258df60b1b76c370370707
Prince Narteh <prinarteh@gmail.com>
2022-06-08 06:26:17 +0000

    fix: invalid jwt signature

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -4,7 +4,7 @@ export function hashPassword(password: string) {
   const salt = crypto.randomBytes(16).toString("hex");
 
   const hash = crypto
-    .pbkdf2Sync(password, salt, 1000, 64, "SHA512")
+    .pbkdf2Sync(password, salt, 1000, 64, "sha512")
     .toString("hex");
 
   return { hash, salt };
@@ -19,7 +19,7 @@ export function verifyPassword({
   hash: string;
 }): boolean {
   const candidateHash = crypto
-    .pbkdf2Sync(candidatePassword, salt, 1000, 64, "SHA512")
+    .pbkdf2Sync(candidatePassword, salt, 1000, 64, "sha512")
     .toString("hex");
 
   return candidateHash === hash;
