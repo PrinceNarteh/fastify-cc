3ca4e2fe005f14089ce6a7840092a5b2c2a5bb87
Prince Narteh <prinarteh@gmail.com>
2022-06-10 05:39:03 +0000

    feat: adds swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ import productRoutes from "./modules/product/product.route";
 
 import { productSchemas } from "./modules/product/product.schema";
 import userRoutes from "./modules/user/user.route";
+import swagger from "@fastify/swagger";
+import { withRefResolver } from "fastify-zod";
 import { userSchemas } from "./modules/user/user.schema";
+import { version } from "../package.json";
 
 export const server = Fastify();
 
@@ -33,15 +36,33 @@ async function main() {
   for (const schema of [...productSchemas, ...userSchemas]) {
     server.addSchema(schema);
   }
+
+  server.register(
+    swagger,
+
+    withRefResolver({
+      openapi: {
+        info: {
+          title: "Product API",
+          description: "Product API build with fastify",
+          version,
+        },
+      },
+      routePrefix: "/doc",
+      staticCSP: true,
+      exposeRoute: true,
+    })
+  );
+
   // Routes
   server.register(userRoutes, { prefix: "api/users" });
   server.register(productRoutes, { prefix: "api/products" });
 
   try {
-    const address = await server.listen(3000, "0.0.0.0");
+    const address = await server.listen({ port: 3000, host: "0.0.0.0" });
     console.log(`🚀 Server ready at ${address}`);
   } catch (error) {
-    console.error(error);
+    server.log.error(error);
     process.exit(1);
   }
 }
